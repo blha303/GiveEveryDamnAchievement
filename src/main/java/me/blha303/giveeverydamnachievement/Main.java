c98d638744a35fa12fe3f3f10222e6debdec1c81
blha303 <stevensmith.ome@gmail.com>
2013-03-28 18:14:00 +0800

    Merged ScoreUnder's PR. Fixes blha303/GiveEveryDamnAchievement#3

--- src/main/java/me/blha303/giveeverydamnachievement/Main.java
+++ src/main/java/me/blha303/giveeverydamnachievement/Main.java
@@ -5,28 +5,20 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.ChatColor;
 
 public class Main extends JavaPlugin {
 
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
-    {
-        Player p = null;
-        try
-        {
-            p = getServer().getPlayer(args[0]);
-        } 
-        catch (Exception e)
-        {
-            return false;
-        }
-        if (p != null)
-        {
-            for (Achievement ach : Achievement.values())
-            {
-                p.awardAchievement(ach);
-            }
-            return true;
-        }
-        return false;
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    if (args.length != 1) return false;
+    Player p = getServer().getPlayer(args[0]);
+    if (p != null) {
+      for (Achievement ach : Achievement.values()) {
+        p.awardAchievement(ach);
+      }
+    } else {
+      sender.sendMessage(ChatColor.RED + "Couldn't find a player called " + ChatColor.RESET + args[0]);
     }
+    return true;
+  }
 }
