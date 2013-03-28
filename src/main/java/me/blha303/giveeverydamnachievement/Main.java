af8d587a18327b4f89976c80eed21848ab3d1a0c
Ribesg <ribesg@yahoo.fr>
2013-03-28 12:01:51 +0100

    4 spaces indentation

--- src/main/java/me/blha303/giveeverydamnachievement/Main.java
+++ src/main/java/me/blha303/giveeverydamnachievement/Main.java
@@ -9,18 +9,18 @@ import org.bukkit.ChatColor;
 
 public class Main extends JavaPlugin {
 
-  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-    if (args.length < 1) return false;
-    for (String arg : args) {
-      Player p = getServer().getPlayer(arg);
-      if (p != null) {
-        for (Achievement ach : Achievement.values()) {
-          p.awardAchievement(ach);
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length < 1) return false;
+        for (String arg : args) {
+            Player p = getServer().getPlayer(arg);
+            if (p != null) {
+                for (Achievement ach : Achievement.values()) {
+                    p.awardAchievement(ach);
+                }
+            } else {
+                sender.sendMessage(ChatColor.RED + "Couldn't find a player called " + ChatColor.RESET + arg);
+            }
         }
-      } else {
-        sender.sendMessage(ChatColor.RED + "Couldn't find a player called " + ChatColor.RESET + arg);
-      }
+        return true;
     }
-    return true;
-  }
 }
